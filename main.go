101d5404eb31f5bd77f1a9a12a4949ee552ce9d2
Nicolas Accetta <nicolas.accetta@gmail.com>
2024-02-27 15:27:38 -0300

    feat: use a generic as loader

--- main.go
+++ main.go
@@ -10,6 +10,27 @@ type Event struct {
 	Name string
 }
 
+type Head struct {
+	Title string
+	// metas []Meta
+	// links []Link
+	// styles []Style
+	// scripts []Script
+}
+
+type Loader struct {
+	Event Event
+}
+
+type Data[T any] struct {
+	Head   Head
+	Loader T
+}
+
+type HomeLoader struct {
+	Event Event
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		t := template.Must(
@@ -18,7 +39,10 @@ func main() {
 				"src/routes/home.html",
 			),
 		)
-		t.Execute(w, Event{Name: "Jodota 22"})
+		t.Execute(w, Data[HomeLoader]{
+			Head:   Head{Title: "Home page"},
+			Loader: HomeLoader{Event: Event{Name: "Home page"}},
+		})
 	})
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
