46c143cde0f248ce2e49a431f376cbd08f93e4f4
Nicolas Accetta <nicolas.accetta@gmail.com>
2024-02-27 13:48:57 -0300

    feat: second commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Event struct {
+	Name string
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t := template.Must(template.ParseFiles("src/root.html", "src/routes/home.html"))
+		t.Execute(w, Event{Name: "Jodota 22"})
+	})
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
